6c8db2d7ea2ea3341f4a844067139e7a527c6287
WesMcW <wesmcwhorter@gmail.com>
2020-10-03 11:31:46 -0500

    Added folders

--- CoolGame/Assets/Scripts/Player/CameraLook.cs
+++ CoolGame/Assets/Scripts/Player/CameraLook.cs
@@ -9,13 +9,11 @@ public class CameraLook : MonoBehaviour
 
     float xRotation = 0f;
 
-    // Start is called before the first frame update
     void Start()
     {
         Cursor.lockState = CursorLockMode.Locked;
     }
 
-    // Update is called once per frame
     void Update()
     {
         float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
