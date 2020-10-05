b26582d0ce6e9e189f7f4580b1a60ddf624995e3
Leah Blasczyk <BlasczykLeah@users.noreply.github.com>
2020-10-05 16:28:33 -0500

    uh

--- CoolGame/Assets/Scripts/GameManager.cs
+++ CoolGame/Assets/Scripts/GameManager.cs
@@ -41,7 +41,7 @@ public class GameManager : MonoBehaviour
         activeRoom.myTimer.GetComponent<TextMeshProUGUI>().text = SecondsToTime(gameTime);
         if (activeRoom.isHall) activeRoom.nextRoom.myTimer.GetComponent<TextMeshProUGUI>().text = SecondsToTime(gameTime);
 
-        if (Input.GetKeyDown(KeyCode.Escape))
+        if (Input.GetKeyDown(KeyCode.Escape) || Input.GetKeyDown(KeyCode.P))
         {
             paused = !paused;
             pauseMenu.SetActive(paused);
